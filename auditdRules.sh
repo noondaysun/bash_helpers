65587ecb0c5eb7d188ee5f047b2a5a59d2fd256c
Feighen Oosterbroek <feighen.oosterbroek@cashrewards.com>
2018-04-12 14:31:18 +0200

    Run auditctl rules for docker

--- auditdRules.sh
+++ auditdRules.sh
@@ -7,5 +7,5 @@ auditctl -w /etc/docker -p wa
 auditctl -w /etc/default/docker -p wa
 auditctl -w /usr/bin/docker-containerd -p wa
 auditctl -w /usr/bin/docker-runc -p wa
-auditctl -w docker.service -p wa
-auditctl -w docker.socket -p wa
+auditctl -w /lib/systemd/system/docker.service -p wa
+auditctl -w /lib/systemd/system/docker.socket -p wa
